1ec56a484df363dd2cafdef1eac0165e63b9aac1
natan <ochenoficialniy@gmail.com>
2026-01-18 21:38:49 +0200

    gallery code rework

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -40,3 +40,39 @@ export function showLoader() {
 export function hideLoader() {
   loader.classList.add("hidden");
 }
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              loading="lazy"
+            />
+          </a>
+          <div class="info">
+            <p class="info-item"><b>Likes</b> ${likes}</p>
+            <p class="info-item"><b>Views</b> ${views}</p>
+            <p class="info-item"><b>Comments</b> ${comments}</p>
+            <p class="info-item"><b>Downloads</b> ${downloads}</p>
+          </div>
+        </li>
+      `
+    )
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}

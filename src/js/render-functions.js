4683536419cadaf22a253d88500e490a4169c5df
natan <ochenoficialniy@gmail.com>
2026-01-18 21:42:19 +0200

    gallery code rework with fixed creategallery function

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -12,38 +12,6 @@ const lightbox = new SimpleLightbox(".gallery a", {
 export function createGallery(images) {
   const markup = images
     .map(
-      image => `
-      <a href="${image.largeImageURL}">
-        <img 
-          src="${image.webformatURL}" 
-          alt="${image.tags}" 
-          width="360" 
-          height="200"
-        />
-      </a>
-    `
-    )
-    .join("");
-
-  gallery.insertAdjacentHTML("beforeend", markup);
-  lightbox.refresh();
-}
-
-export function clearGallery() {
-  gallery.innerHTML = "";
-}
-
-export function showLoader() {
-  loader.classList.remove("hidden");
-}
-
-export function hideLoader() {
-  loader.classList.add("hidden");
-}
-
-export function createGallery(images) {
-  const markup = images
-    .map(
       ({
         webformatURL,
         largeImageURL,
@@ -76,3 +44,17 @@ export function createGallery(images) {
   galleryContainer.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
 }
+
+
+export function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+export function showLoader() {
+  loader.classList.remove("hidden");
+}
+
+export function hideLoader() {
+  loader.classList.add("hidden");
+}
+

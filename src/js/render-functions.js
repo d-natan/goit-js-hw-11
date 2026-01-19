46e074395caa9fa29dd24b0f8222c9d3bef07fea
natan <ochenoficialniy@gmail.com>
2026-01-19 17:50:03 +0200

    render functions loader updated code

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,11 +2,20 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
+}
 
 export function createGallery(images) {
   const markup = images
@@ -30,10 +39,10 @@ export function createGallery(images) {
             />
           </a>
           <div class="info">
-            <p class="info-item"><b>Likes</b> ${likes}</p>
-            <p class="info-item"><b>Views</b> ${views}</p>
-            <p class="info-item"><b>Comments</b> ${comments}</p>
-            <p class="info-item"><b>Downloads</b> ${downloads}</p>
+            <p><b>Likes</b> ${likes}</p>
+            <p><b>Views</b> ${views}</p>
+            <p><b>Comments</b> ${comments}</p>
+            <p><b>Downloads</b> ${downloads}</p>
           </div>
         </li>
       `
